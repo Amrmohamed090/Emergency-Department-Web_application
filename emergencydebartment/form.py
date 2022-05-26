cecea5aaad2bde3501f0785b739a526875875e3d
Amrmohamed090 <93833053+Amrmohamed090@users.noreply.github.com>
2022-05-26 23:38:56 +0200

    Update form.py

--- emergencydebartment/form.py
+++ emergencydebartment/form.py
@@ -1,12 +1,19 @@
-from wsgiref.validate import validator
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, EmailField,IntegerField,DateField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, EmailField,IntegerField,DateField, RadioField
+from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
+from emergencydebartment.models import Doctor, Patient, Admin
+
+
+
+
+
+
 
 
 class LoginForm(FlaskForm):
-
-    username = StringField('Username', validators=[
+    
+    email = StringField('Email', validators=[
                            DataRequired(), Length(min=2, max=20)])
     password = PasswordField('Password', validators=[
                              DataRequired(), Length(min=1, max=30)])
@@ -14,18 +21,31 @@ class LoginForm(FlaskForm):
     remember = BooleanField('remember Me')
 
 
-class Signup_DoctorForm(FlaskForm):
+class RegistrationForm(FlaskForm):
     
-    Fname = StringField('First-name', validators=[DataRequired(), Length(min=2, max=20)])
-    
-    Lname = StringField('Last-name', validators=[DataRequired(), Length(min=2, max=20)])
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    degree = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    birthdate = DateField('Birthdate', validators=[DataRequired()])
+    Fname = StringField('First Name', validators=[DataRequired(), Length(min=1, max=30)])
+    Sname = StringField('Middle Name', validators=[Length(min=1, max=30)])
+    Lname = StringField('Last Name', validators=[DataRequired(),Length(min=1, max=30)])
+
     email = StringField('Email', validators=[DataRequired(), Email()])
-    salary = IntegerField('Salary', validators=[DataRequired()])
-    degree = StringField('Degree', validators=[DataRequired(), Length(min=5, max=30)])
-    speciallity = StringField('Major-Scientific-area', validators=[DataRequired(), Length(min=5, max=30)])
-    
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=30)])
+    birth_date = DateField('BirthDate')
+    salary = IntegerField("Salary",  validators=[DataRequired()])
+    gender = RadioField(choices=["male","female"])
+    speciality = StringField('Speciality', validators=[DataRequired(), Length(min=1, max=30)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=1, max=30)])
+    confirm_password = PasswordField('Confirm password',validators=[DataRequired(),EqualTo('password')])
     submit = SubmitField('sign up')
+
+    def validate_username(self, username):
+        user1 =  Doctor.query.filter_by(username=username.data).first()
+        user2 =  Patient.query.filter_by(username=username.data).first()
+        user3 = Admin.query.filter_by(username=username.data).first()
+        if user1 and user2:
+            raise ValidationError('username is taken, please choose another one')
+
+    def validate_email(self, email):
+        email1 =  Doctor.query.filter_by(email=email.data).first()
+        if email1:
+            raise ValidationError('email is taken, please choose another one')
+
+
